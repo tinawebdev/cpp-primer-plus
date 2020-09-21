d73dbb4c07662a0871b1e298c823548a5b7f22c2
Kristina <codingkris@yahoo.com>
2020-09-21 14:09:37 +0300

    Added example from chapter 03

---
+++ chapter03/limits.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+int main()
+{
+    using namespace std;
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+
+    cout << "int is " << sizeof(int) << " bytes." << endl;
+    cout << "short is " << sizeof n_short << " bytes." << endl;
+    cout << "long is " << sizeof n_long << " bytes." << endl;
+    cout << "long long is " << sizeof n_llong << " bytes." << endl;
+    cout << endl;
+
+    cout << "Maximum values:" << endl;
+    cout << "int: " << n_int << endl;
+    cout << "short: " << n_short << endl;
+    cout << "long: " << n_long << endl;
+    cout << "long long: " << n_llong << endl << endl;
+
+    cout << "Minimum int value = " << INT_MIN << endl;
+    cout << "Bits per byte = " << CHAR_BIT << endl;
+    return 0;
+}
