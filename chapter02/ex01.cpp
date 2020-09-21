3356050f3706c9d3be48c72d4da48a6982aa76b4
Kristina <codingkris@yahoo.com>
2020-09-21 11:43:39 +0300

    Completed exercise 01

---
+++ chapter02/ex01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "My name is Kristina" << std::endl;
+    std::cout << "Saint Petersburg, Russia" << std::endl;
+
+    std::cin.get();
+    return 0;
+}
