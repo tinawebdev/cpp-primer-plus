4acd6f61f02ff12b56c345c16eb2ee854d03ce44
Kristina <codingkris@yahoo.com>
2020-09-22 11:55:17 +0300

    Completed exercise 05

---
+++ chapter03/ex05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Enter the world's population:" << std::endl;
+    unsigned long long worldPopulation;
+    std::cin >> worldPopulation;
+    std::cout << "Enter the population of the US:" << std::endl;
+    unsigned long long countryPopulation;
+    std::cin >> countryPopulation;
+    double result = double(countryPopulation) * 100.0f / worldPopulation;
+    std::cout << "The population of the US is " << result 
+              << "% of the world population." << std::endl;
+
+    std::cin.get();
+    std::cin.get();
+    return 0;
+}
