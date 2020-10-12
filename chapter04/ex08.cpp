fcf5ba6a95481508e828b6c06eb4ce875011cc9a
Kristina <codingkris@yahoo.com>
2020-10-12 12:26:27 +0300

    Completed exercise 08

---
+++ chapter04/ex08.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+struct pizza
+{
+    std::string companyName;
+    int diameter;
+    float weight;
+};
+
+int main()
+{
+    pizza *pepperoni = new pizza;
+
+    std::cout << "Enter the diameter: ";
+    std::cin >> pepperoni->diameter;
+    std::cin.get();
+
+    std::cout << "Enter the company name: ";
+    std::getline(std::cin, pepperoni->companyName);
+
+    std::cout << "Enter the weight: ";
+    std::cin >> pepperoni->weight;
+
+    std::cout << std::endl;
+
+    std::cout << "Company name: " << pepperoni->companyName << std::endl;
+    std::cout << "Diameter: " << pepperoni->diameter << std::endl;
+    std::cout << "Weight: " << pepperoni->weight << std::endl;
+
+    delete pepperoni;
+
+    std::cin.get();
+    return 0;
+}
