c61a865d91206ea9413663a7752c229b40b806b9
Kristina Goncharova <codingkris@yahoo.com>
2020-10-13 13:33:05 +0300

    Completed exercise 01

---
+++ chapter05/ex01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int val1, val2, result = 0;
+
+	std::cout << "Enter 2 integers. Enter the smaller first: ";
+	std::cin >> val1;
+	std::cout << "Enter the second number: ";
+	std::cin >> val2;
+
+	for (int i = val1; i <= val2; i++)
+	{
+		result += i;
+	}
+
+	std::cout << std::endl;
+	std::cout << "Result: " << result << std::endl;
+
+	return 0;
+}
