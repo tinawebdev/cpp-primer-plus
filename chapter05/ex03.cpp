827fac539f551e41955e4da416fc131cccce3b15
Kristina Goncharova <codingkris@yahoo.com>
2020-10-13 13:47:56 +0300

    Completed exercise 03

---
+++ chapter05/ex03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int num, sum = 0;
+
+	do {
+		cout << "Enter a number (0 to stop): ";
+		cin >> num;
+		sum += num;		
+		cout << "Result: " << sum << endl;
+	} while (num);	
+
+	return 0;
+}
