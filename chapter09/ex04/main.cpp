5d3fa2d81643c090ded60070cfffb671a32545d1
Kristina Goncharova <codingkris@yahoo.com>
2020-12-04 13:14:31 +0300

    Code style fixed

--- chapter09/ex04/main.cpp
+++ chapter09/ex04/main.cpp
@@ -2,17 +2,17 @@
 #include "namespace.h"
 
 int main() {
-	using namespace SALES;
-	Sales s1, s2;
-	double ar[4] = {1, 2, 3, 4};
-	
-	setSales(s1, ar, (sizeof(ar) / sizeof(double)));
-	setSales(s2);
-	
-	std::cout << "Displaying contents of struct s1" << std::endl;
-	showSales(s1);
-	std::cout << "Displaying contents of struct s2" << std::endl;
-	showSales(s2);
-	
-	return 0;
+  using namespace SALES;
+  Sales s1, s2;
+  double ar[4] = {1, 2, 3, 4};
+  
+  setSales(s1, ar, (sizeof(ar) / sizeof(double)));
+  setSales(s2);
+  
+  std::cout << "Displaying contents of struct s1" << std::endl;
+  showSales(s1);
+  std::cout << "Displaying contents of struct s2" << std::endl;
+  showSales(s2);
+  
+  return 0;
 }
